4a24d14d56d08db7202fa6187c4ee372331a6610
F1liskos <185220670+F1liskos@users.noreply.github.com>
2025-05-20 09:51:02 +0300

    Link Fix

--- script.js
+++ script.js
@@ -52,10 +52,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
-
-var typed = new Typed("#typing", {
-  strings: ["Designer", "Developer", "Freelancer"],
-  typeSpeed: 100,
-  backSpeed: 60,
-  loop: true,
-});
